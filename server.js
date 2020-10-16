10ad4a0db24cc5410b1d4cec0557f65a47d0c358
Marcelo Rocha Ferreira <marcelo@cpd.pmvc.ba.gov.br>
2020-10-16 16:30:07 -0300

    Criando as rotas.

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const fileupload = require('express-fileupload');
 
+const apiRoutes = require('./src/routes');
+
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useFindAndModify:false,
@@ -25,9 +27,7 @@ server.use(fileupload());
 
 server.use(express.static(__dirname + '/public'));
 
-server.get('/ping', (req, res) => {
-    res.json({pong: true});
-});
+server.use('/', apiRoutes);
 
 server.listen(process.env.PORT, () => {
     console.log(`- Executando no endereço: ${process.env.BASE}`);
